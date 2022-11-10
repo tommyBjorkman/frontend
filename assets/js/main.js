b96cee3c34f3717512a31cc97a9a609f56f99db3
tommyBjorkman <git-autoconfig>
2022-11-10 07:08:12 +0100

    startededitingfor loop in main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 const url = 'https://api.punkapi.com/v2/beers/';
 
-console.log(url);
+let data = fetch(url)
+   .then(res => res.json())
+   .then(data => {
+        for(let i = 0; i < data.length; i++){
+            
+        }
+   });
