3bd047ac663b08837dbc7275d7eff0e80c30b760
tommyBjorkman <git-autoconfig>
2022-11-11 13:29:53 +0100

    added example cart

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,3 +35,61 @@ let data = fetch(url)
     };
     cardBeer.innerHTML = output;
   })
+
+  let count = 0;
+let sum = 0;
+let cart = {};
+
+if (localStorage.getItem("count")) {
+    count = parseInt(localStorage.getItem("count"));
+}
+
+if (localStorage.getItem("sum")) {
+    sum = parseInt(localStorage.getItem("sum"));
+}
+
+if (localStorage.getItem("cart")) {
+    cart = JSON.parse(localStorage.getItem("cart"));
+}
+
+updateCart();
+
+let btns = document.querySelectorAll(".products button");
+
+for (let i = 0; i < btns.length; i++) {
+    let btn = btns[i];
+    btn.addEventListener("click", add);
+    id = btn.dataset.id;
+}
+
+function add(event) {
+    let price = Number(event.target.dataset.price);
+    console.log(event)
+    let title = event.target.dataset.title;
+    let id = event.target.dataset.id;
+
+    if (id in cart) {
+        cart[id].qty++;
+    } else {
+        let cartItem = {
+            title: title,
+            price: price,
+            qty: 1
+        };
+        cart[id] = cartItem
+    }
+
+    count++;
+    sum += price;
+
+    console.log(cart);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    updateCart();
+}
+
+function updateCart() {
+    document.getElementById("sum").textContent = sum;
+    document.getElementById("count").textContent = count;
+    localStorage.setItem("sum", sum);
+    localStorage.setItem("count", count);
+}
